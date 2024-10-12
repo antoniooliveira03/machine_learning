db393aaab1ef67937a7c83443965e718db5e15e9
antoniooliveira03 <tzpoliveira@gmail.com>
2024-10-12 10:29:35 +0100

    missing values cont, outliers

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
-def plot_histogram(data, xlabel, ylabel, title, color='blue'):
+def plot_histogram(data, xlabel, ylabel, title, color='lightblue'):
     """
     Plots a histogram of the given data.
 
@@ -9,10 +10,32 @@ def plot_histogram(data, xlabel, ylabel, title, color='blue'):
         xlabel (str): The label for the x-axis.
         ylabel (str): The label for the y-axis.
         title (str): The title of the histogram.
-        color (str): The color of the histogram bars. Default is 'blue'.
+        color (str): The color of the histogram bars. Default is 'lightblue'.
     """
     plt.hist(data, edgecolor='black', color=color)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
     plt.show()
+
+
+def boxplots(data, color='lightblue'):
+    """
+    Generate boxplotsfor numeric columns in the provided data.
+
+    Args:
+      data (pandas.DataFrame): The input data containing numeric columns.
+      graph (str): The type of graph to generate. Options: 'boxplot' or 'histogram'.
+      color (str): The color of the plot. Default is 'lightblue'.
+    """
+
+    for column in data.columns:
+        # Check if the column is numeric
+        if pd.api.types.is_numeric_dtype(data[column]):
+            plt.boxplot(data[column], vert=False, patch_artist=True, boxprops=dict(facecolor=color))
+            plt.title(f'Boxplot of {column}')
+            plt.yticks([])
+            plt.show()
+            
+        else:
+            continue
