31fe7bec4f82fc35a4135d51937434117021bfc8
antoniooliveira03 <tzpoliveira@gmail.com>
2024-10-30 16:06:45 +0000

    feature selct

--- feature_selection.py
+++ feature_selection.py
@@ -5,6 +5,13 @@ from sklearn.linear_model import Lasso
 from sklearn.feature_selection import RFE
 from sklearn.metrics import classification_report
 
+# CHI-SQUARED
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.preprocessing import MinMaxScaler
+
+# MUTUAL INFORMATION 
+from sklearn.feature_selection import mutual_info_classif
+
 # OTHER
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,7 +19,7 @@ import numpy as np
 import pandas as pd
 
 # LASSO
-def lasso_feature_importance(X, y, num, alpha = 0.01, color = 'Blue'):
+def lasso(X, y, num, alpha = 0.01, color = 'Blue'):
     X_num = X[num] 
     
     lasso = Lasso(alpha=alpha)
@@ -60,3 +67,91 @@ def rfe(X, y, num, n_features, model=None):
         results[feature] = selected_features
         
     return results
+
+# CORRELATION MATRIX
+
+def correlation_matrix(X, num, threshold=0.8, cmap = 'Blues'):
+    
+    X_num = X[num]
+    
+    # Compute the absolute correlation matrix
+    corr_matrix = X_num.corr().abs()
+
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(corr_matrix, annot=True, cmap=cmap, fmt=".2f")
+    plt.title("Feature Correlation Matrix")
+    plt.show()
+    
+    # Set to track features to drop
+    to_drop = set()
+    
+    # Iterate over columns to find highly correlated pairs
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i + 1, len(corr_matrix.columns)):
+            feature1 = corr_matrix.columns[i]
+            feature2 = corr_matrix.columns[j]
+            
+            # Check if the correlation is above the threshold
+            if corr_matrix.iloc[i, j] > threshold:
+                # Add the second feature to the drop list
+                to_drop.add(feature2)
+    
+    # List of selected features: all features except those in to_drop
+    selected_features = [feature for feature in X_num.columns if feature not in to_drop]
+    
+    return selected_features, to_drop
+
+# UNIVARIATE TEST
+def var(X, num, threshold=0.01):
+    X_num = X[num]
+    # Calculate variance for each feature
+    variances = X_num.var()
+    # Select features with variance above the threshold
+    selected_features = variances[variances > threshold].index
+    
+    return selected_features.tolist()
+
+######################## CATEG ########################
+
+# CHI-SQUARED
+def chi_squared(X, y, categ, threshold=0.05):
+    
+    X_categ = X[categ]
+    
+    scaler = MinMaxScaler()
+    X_scaled = pd.DataFrame(scaler.fit_transform(X_categ), 
+                            columns=X_categ.columns)
+    
+    # Fit the chi-squared selector
+    chi2_selector = SelectKBest(chi2, k='all')
+    chi2_selector.fit(X_scaled, y)
+
+    # Get Chi-squared scores
+    chi2_scores = chi2_selector.scores_
+
+    # Create a DataFrame for scores
+    scores_df = pd.DataFrame({
+        'Feature': X_categ.columns,
+        'Chi2 Score': chi2_scores
+    })
+
+    # Filter features based on the threshold
+    selected_features = scores_df[scores_df['Chi2 Score'] > threshold]['Feature']
+    
+    return scores_df, selected_features.tolist()
+
+# MUTUAL INFORMATION 
+def mutual_information(X, y, categ, threshold=0.1):
+    
+    X_categ = X[categ]
+    # Calculate Mutual Information
+    mi_scores = mutual_info_classif(X_categ, y, discrete_features='auto', random_state=42)
+    
+    # Create a DataFrame for better visualization
+    mi_scores_df = pd.DataFrame(mi_scores, index=X_categ.columns, columns=["Mutual Information"])
+    mi_scores_df = mi_scores_df.sort_values(by="Mutual Information", ascending=False)
+
+     # Filter features based on the threshold
+    selected_features = mi_scores_df[mi_scores_df["Mutual Information"] >= threshold].index.tolist()
+    
+    return mi_scores_df, selected_features
