dc13f0eefed2270f3f2a75265fb58a2c1976d7a8
antoniooliveira03 <tzpoliveira@gmail.com>
2024-12-16 09:36:11 +0000

    stuff

--- delivery 2 (in progress)/models.py
+++ delivery 2 (in progress)/models.py
@@ -10,9 +10,7 @@ import utils2 as p
 from sklearn.preprocessing import (
     StandardScaler,
     MinMaxScaler,
-    RobustScaler
-)
-
+    RobustScaler)
 
 # Models
 from sklearn.linear_model import LogisticRegression, SGDClassifier
@@ -24,7 +22,6 @@ from lightgbm import LGBMClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-
 
 # Metrics
 from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
